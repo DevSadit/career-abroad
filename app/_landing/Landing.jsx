7267256d9f48c06c7e288e574acbdfa9a8e0fed4
DevSadit <dev.sadit@gmail.com>
2026-01-02 02:55:58 +0600

    basic mentors section added

--- app/_landing/Landing.jsx
+++ app/_landing/Landing.jsx
@@ -3,12 +3,14 @@ import Hero from "./hero/Hero";
 import About from "./about/About";
 import CoursesSection from "./courses/CoursesSection";
 import Faq from "./faq/Faq";
+import Mentors from "./mentors/Mentors";
 const Landing = () => {
   return (
     <div className="">
       <Hero />
       <About />
       <CoursesSection />
+      <Mentors />
       <Faq />
     </div>
   );
--- app/_landing/faq/Faq.jsx
+++ app/_landing/faq/Faq.jsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 import Image from "next/image";
 import React from "react";
 import countries from "../../../../data/countries.json";
@@ -77,19 +77,6 @@ const Faq = () => {
           ))}
         </div>
       </div>
-
-      <style jsx>{`
-        @keyframes fadeInUp {
-          from {
-            opacity: 0;
-            transform: translateY(30px);
-          }
-          to {
-            opacity: 1;
-            transform: translateY(0);
-          }
-        }
-      `}</style>
     </section>
   );
 };
